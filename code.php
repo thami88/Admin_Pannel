7ebfdc8c79b24e8e5a176dd0a07fd81e7ae4c713
Thamindu <thamindu888@gmail.com>
2020-10-25 18:26:09 +0530

    Initial Commit

---
+++ code.php
@@ -0,0 +1,54 @@
+<?php
+// include('security.php');
+
+session_start();
+
+// DB Connection
+$connection = mysqli_connect("localhost","root","","adminpanel");
+
+if(isset($_POST['registerbtn']))
+{
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['confirmpassword'];
+
+    // $email_query = "SELECT * FROM register WHERE email='$email' ";
+    // $email_query_run = mysqli_query($connection, $email_query);
+    // if(mysqli_num_rows($email_query_run) > 0)
+    // {
+    //     $_SESSION['status'] = "Email Already Taken. Please Try Another one.";
+    //     $_SESSION['status_code'] = "error";
+    //     header('Location: register.php');  
+    // }
+    // else
+    // {
+        // Confirm Password
+        if($password === $cpassword)
+        {
+            $query = "INSERT INTO register (username,email,password) VALUES ('$username','$email','$password')";
+            $query_run = mysqli_query($connection, $query);
+            
+            if($query_run)
+            {
+                // echo "Saved";
+                $_SESSION['success'] = "Admin Profile Added";
+                // $_SESSION['status_code'] = "success";
+                header('Location: register.php');
+            }
+            else 
+            {
+                $_SESSION['status'] = "Admin Profile Not Added";
+                // $_SESSION['status_code'] = "error";
+                header('Location: register.php');  
+            }
+        }
+        else 
+        {
+            $_SESSION['status'] = "Password and Confirm Password Does Not Match";
+            // $_SESSION['status_code'] = "warning";
+            header('Location: register.php');  
+        }
+    // }
+
+}
